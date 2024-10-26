b01b2b47d52168767fd0bd38db200d7247dfdb9d
Quaderatics <155383076+Quaderatics@users.noreply.github.com>
2024-10-26 15:12:45 -0700

    Add `write_pack` script

---
+++ write_pack.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+rm -rf ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8/datapacks/Mineshaft\ 8
+cp -r ./Mineshaft\ 8 ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8/datapacks
