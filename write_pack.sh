e7b706ae20e5b55c970b7262a11d092471cb3b0d
Quaderatics <155383076+Quaderatics@users.noreply.github.com>
2024-11-24 23:50:44 -0800

    Add pack icons, zip both packs in script

--- write_pack.sh
+++ write_pack.sh
@@ -1,4 +1,14 @@
 #!/bin/zsh
 
-rm -rf ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8/datapacks/Mineshaft\ 8
-cp -r ./Mineshaft\ 8 ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8/datapacks
+# Data pack
+rm -rf ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8/datapacks/Mineshaft\ 8.zip
+cd ./Mineshaft\ 8
+zip -r 'Mineshaft 8' . -x '__MACOSX' -x '.DS_Store' -x '**/.DS_Store'
+mv ./Mineshaft\ 8.zip ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8/datapacks
+cd ..
+
+# Resource pack
+rm -rf ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8/resources.zip
+cd ./resources
+zip -r resources . -x '__MACOSX' -x '.DS_Store' -x '**/.DS_Store'
+mv ./resources.zip ~/Library/Application\ Support/minecraft/saves/Mineshaft\ 8
